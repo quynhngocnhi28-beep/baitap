fac69804453fcc594019bb08380b7d1dac8ba5fb
quynhngocnhi28-beep <quynhngocnhi28@gmail.com>
2026-03-16 10:22:38 +0700

    Add multiple files

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import React, { useState } from "react";
 import Login from "./components/Login";
 import Register from "./components/Register";
 
